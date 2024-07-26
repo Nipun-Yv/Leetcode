4fad584129989be049681e193f935d662f748eeb
Nipun Yadav <nipunyadav@Nipuns-MacBook-Air.local>
2024-07-26 22:15:35 +0530

    Connected the index.js script and index.css stylesheet with the index.ejs file

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express"
 import bodyParser from "body-parser"
 const port=3000;
 const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("./public"))
 app.listen(port,()=>{
     console.log("Server running successfully and recieving requests from port "+port);
 })
