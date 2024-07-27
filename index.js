179d286a57685183bda37f4278ddc2b899f5ee6d
Nipun Yadav <nipunyadav@Nipuns-MacBook-Air.local>
2024-07-28 04:30:34 +0530

    Made the grid, created the badges section, it make take a few hours to upload, computer getting updated

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.post("/profile", async(req,res)=>{
     const responseB=await axios.get(`https://alfa-leetcode-api.onrender.com/${req.body.username}/badges`);
     const resultB=responseB.data;
     if(resultB.errors){throw new Error("Dudeeeee")}
+    var badges=generateImgURLS(resultB.badges);
     const responseC=await axios.get(`https://alfa-leetcode-api.onrender.com/${req.body.username}/contest`);
     const resultC=responseC.data;
     if(!resultC.contestAttend){
@@ -27,7 +28,7 @@ app.post("/profile", async(req,res)=>{
         mediumSolved:result.mediumSolved,
         hardSolved:result.hardSolved,
         ranking:result.ranking,
-        badges:resultB.badges, //(object List) Has an id attribute, an icon attribute(Image link), has a displayName
+        badges:badges, //(object List) Has an id attribute, an icon attribute(Image link), has a displayName
     });}
     else{
         res.render("index.ejs",{
@@ -36,7 +37,7 @@ app.post("/profile", async(req,res)=>{
             mediumSolved:result.mediumSolved,
             hardSolved:result.hardSolved,
             ranking:result.ranking,
-            badges:resultB.badges,
+            badges:badges,
             contestAttend: resultC.contestAttend,
             contestRating: resultC.contestRating,
             contestGlobalRanking: resultC.contestGlobalRanking,
@@ -50,3 +51,23 @@ app.post("/profile", async(req,res)=>{
         res.render("login.ejs",{err:"Username does not exist"});
     }
 })
+function generateImgURLS(badges2){
+    var map={"50 Days Badge 2024":"/images/50Days.gif","100 Days Badge 2024":"/images/100Days.webp",
+        "Jul LeetCoding Challenge":"",
+        "Jun LeetCoding Challenge":"/images/Jun.gif",
+        "May LeetCoding Challenge":"/images/May.gif.jpeg",
+        "Apr LeetCoding Challenge":"/images/Apr.gif",
+        "Mar LeetCoding Challenge":"/images/Mar.gif.jpeg",
+        "Feb LeetCoding Challenge":"/images/Feb.gif.jpeg",
+        "Jan LeetCoding Challenge":"/images/Jan.gif"
+    }
+    let badges=[];
+    for(var i=0;i<badges2.length;i++){
+        console.log(badges2[i].displayName)
+        if(map[badges2[i].displayName]){
+            badges.push(map[badges2[i].displayName]);
+        }
+    }
+    console.log(badges)
+    return badges;
+}
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,1 +1,11 @@
-document.querySelector("h1").innerHTML="Hello"
+//Javascript code for the badges section
+let i=0;
+badges=Number(badges);
+console.log(badges);
+    document.querySelector(`.badges>img:nth-child(${i+1})`).removeAttribute("hidden"); 
+    document.querySelector(".badges>button").addEventListener("click",()=>{
+        document.querySelector(`.badges>img:nth-child(${i+1})`).setAttribute("hidden","true")
+        i=(i+1)%badges;
+        document.querySelector(`.badges>img:nth-child(${i+1})`).removeAttribute("hidden")
+    })
+//End of Javascript code for the badges section
